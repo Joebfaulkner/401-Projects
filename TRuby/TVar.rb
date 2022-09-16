719b56422f7b25efb7fe45f6fdf79a231f47e9a0
Joseph Faulkner <joebfaulkner@addiguru.com>
2022-09-15 19:22:06 -0500

    TVar

---
+++ TRuby/TVar.rb
@@ -0,0 +1,18 @@
+# TVar
+# Joseph Faulkner, Ismael Reyes, Grant Simon, and Hannah Bridevaux
+# Ruby does not require data types to be declared
+
+$IntVar = 5
+$DoubleVar = 0.05
+$FloatVar = 5.55555
+$BooleanVar = true
+$StringVar = "This is a variable"
+
+puts("IntVar = #$IntVar")
+puts("DoubleVar = #$DoubleVar")
+puts("FloatVar = #$FloatVar")
+puts("BooleanVar = #$BooleanVar")
+puts("StringVar contains the string #$StringVar")
+puts("IntVar + DoubleVar = #{$IntVar + $DoubleVar}")
+puts("FloatVar + DoubleVar = #{$FloatVar + $DoubleVar}")
+puts("FloatVar + DoubleVar + IntVar = #{$FloatVar + $DoubleVar + $IntVar}")
